37e35880041937ab3cfbeb484613acca70d9f570
Vignesh_Ss <56245530+vigneshSs-07@users.noreply.github.com>
2022-01-16 03:02:37 +0530

    actual view code

--- BigQuery/demo.py
+++ BigQuery/demo.py
@@ -1,1 +1,19 @@
+from google.cloud import bigquery
+#Creating an instance for bigquery client
+client = bigquery.Client()
 
+#View location to be stored
+view_id = "datalab-336505.demodata.02cancer_view"
+#Source table location
+source_id = "datalab-336505.demodata.cancer"
+
+#https://googleapis.dev/python/bigquery/latest/generated/google.cloud.bigquery.table.Table.html?highlight=bigquery%20table#google.cloud.bigquery.table.Table
+view = bigquery.Table(view_id)
+# SQL query defining the table as a view (defaults to None).
+view.view_query = f"SELECT mean_radius,mean_texture,mean_perimeter,target FROM `{source_id}` limit 10 "
+# https://googleapis.dev/python/bigquery/latest/generated/google.cloud.bigquery.client.Client.html?highlight=create_table#google.cloud.bigquery.client.Client.create_table
+view = client.create_table(view)
+
+# Make an API request to create the view.
+if __name__ == "__main__":    
+    print(f"Created {view.table_type}: {str(view.reference)}")
